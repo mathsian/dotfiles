2755927299a340cdf01b45493a792a6c62da0a4d
ian <ian@iceking.ooo.lan>
2019-04-14 22:27:17 +0100

    fix xmobar wifi

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,20 +1,30 @@
 import XMonad
+import XMonad.Hooks.SetWMName
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.DynamicLog
 import XMonad.Util.Run(spawnPipe)
 import System.IO
+
+xmobarTitleColor = "#22CCDD"
+xmobarCurrentWorkspaceColor = "#CEFFAC"
+myNormalBorderColor  = "black"
+myFocusedBorderColor = "#22CCDD"
+
 main = do
         xmproc <- spawnPipe "xmobar ~/.xmobarrc"
         xmonad $ docks $ defaultConfig
                 { terminal = "kitty"
                 , modMask = mod4Mask
-                , borderWidth = 2
-                , normalBorderColor = "#cccccc"
-                , focusedBorderColor = "#cd8b00"
+                , borderWidth = 1
+                , normalBorderColor = myNormalBorderColor
+                , focusedBorderColor = myFocusedBorderColor
                 , manageHook = manageDocks <+> manageHook defaultConfig
                 , layoutHook = avoidStruts $ layoutHook defaultConfig
-                , logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "hi" . shorten 50
-                        }
-                }
+                , logHook = dynamicLogWithPP $ xmobarPP {
+                    ppOutput = hPutStrLn xmproc
+                  , ppTitle = xmobarColor xmobarTitleColor "" . shorten 100
+                  , ppCurrent = xmobarColor xmobarCurrentWorkspaceColor ""
+                  , ppSep = "   "
+                  }
+                , startupHook = setWMName "LG3D"
+        }
--- bin/start_xmonad.sh
+++ bin/start_xmonad.sh
@@ -4,7 +4,7 @@ xrdb -merge .Xresources
 
 #trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x191970 --height 12 &
 
-xsetroot -solid midnightblue
+xsetroot -solid black
 
 #nm-applet --sm-disable &
 
